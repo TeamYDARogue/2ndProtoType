2ee5e24c5dea0c9f9bbec36a0dfd9d50a1ebe1d7
Zetto <usainfection@gmail.com>
2016-05-30 21:44:33 +0900

    シーケンス制御改良

--- Assets/Scripts/Manager/SequenceManager.cs
+++ Assets/Scripts/Manager/SequenceManager.cs
@@ -1,9 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class SequenceManager : MonoBehaviour {
 
-    private enum State
+    public enum GameState
     {
         eKeyInput = 0,      //キー入力
         ePlayerAction,      //プレイヤーの行動
@@ -11,23 +12,111 @@ public class SequenceManager : MonoBehaviour {
         ePlayerActionEnd,   //プレイヤーの行動終了
         eEnemyAI,           //敵のAIリクエスト
         eEnemyAction,       //敵の行動
+        eEnemyMove,         //敵の移動
         eEnemyActionEnd,    //敵の行動終了
         eMenu,              //メニューを開いている
         eTurnEnd,           //全体のターン終了
         eNextFloor,         //次の階層へ移動
     }
 
-    private State m_nowState = State.eKeyInput;
-    private State m_prevState = State.eKeyInput;
+    private delegate GameState GameProc();
+    private Dictionary<GameState, GameProc> m_gameProc = new Dictionary<GameState, GameProc>();
 
-    void ChangeState(State _state)
+    public static GameState NowState
     {
-        m_prevState = m_nowState;
-        m_nowState = _state;
+        get;
+        set;
+    }
+
+
+    void Awake()
+    {
+        m_gameProc.Add(GameState.eKeyInput, KeyInput);
+
+        m_gameProc.Add(GameState.ePlayerMover, PlayerMove);
+        m_gameProc.Add(GameState.ePlayerAction, PlayerAction);
+        m_gameProc.Add(GameState.ePlayerActionEnd, PlayerActionEnd);
+
+        m_gameProc.Add(GameState.eEnemyAI, EnemyAIReq);
+        m_gameProc.Add(GameState.eEnemyMove, EnemyMove);
+        m_gameProc.Add(GameState.eEnemyAction, EnemyAction);
+        m_gameProc.Add(GameState.eEnemyActionEnd, EnemyActionEnd);
+
+        m_gameProc.Add(GameState.eTurnEnd, TurnEnd);
+
+        NowState = GameState.eKeyInput;
     }
 
     void Update()
     {
+        NowState = m_gameProc[NowState]();
+    }
 
+
+    GameState KeyInput()
+    {
+        Debug.Log(NowState.ToString());
+
+        if(Input.GetAxis("Horizontal")!=0 || Input.GetAxis("Vertical") != 0)
+        {
+            return GameState.ePlayerMover;
+        }
+        return GameState.eKeyInput;
+    }
+
+    GameState PlayerAction()
+    {
+        Debug.Log(NowState.ToString());
+
+        return GameState.ePlayerAction;
+    }
+
+    GameState PlayerMove()
+    {
+        Debug.Log(NowState.ToString());
+
+        return GameState.ePlayerMover;
+    }
+
+    GameState PlayerActionEnd()
+    {
+        Debug.Log(NowState.ToString());
+
+        return GameState.ePlayerActionEnd;
+    }
+
+    GameState EnemyAIReq()
+    {
+        Debug.Log(NowState.ToString());
+
+        return GameState.eEnemyAI;
+    }
+
+    GameState EnemyMove()
+    {
+        Debug.Log(NowState.ToString());
+
+        return GameState.eEnemyMove;
+    }
+
+    GameState EnemyAction()
+    {
+        Debug.Log(NowState.ToString());
+
+        return GameState.eEnemyAction;
+    }
+
+    GameState EnemyActionEnd()
+    {
+        Debug.Log(NowState.ToString());
+
+        return GameState.eEnemyActionEnd;
+    }
+
+    GameState TurnEnd()
+    {
+        Debug.Log(NowState.ToString());
+
+        return GameState.eTurnEnd;
     }
 }
