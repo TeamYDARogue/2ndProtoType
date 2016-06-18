83cdbcee28c95ecba8c6ea2e1bdf2235e4677385
tomytomychoper <iwgm14clow526@gmail.com>
2016-06-18 17:37:08 +0900

    テキストデータ、画像とフィールド関連のソース

---
+++ Assets/Scripts/Field/FieldManager.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Xml;
+using System.Text;
+using System.Collections;
+using System.IO;
+
+public class FieldManager : MonoBehaviour {
+
+    /*
+    FiledManager:フィールド管理
+    MapNumericValue:外部テキスト読み込み
+    Layer2D:データを格納しとく場所
+　　シングルトン化
+    */
+
+    private object mapData;
+    private MapNumericValue mapnumricvalue;
+    private Layer2D layer;
+    private static FieldManager m_instance = null;
+    public static FieldManager Instance
+    {
+        get
+        {
+            if (m_instance == null)
+            {
+                /*
+                compornet 　スクリプトからGameObjectにアタッチするため
+
+                */
+
+                var obj = new GameObject("FieldManager!!");
+                m_instance = obj.AddComponent<FieldManager>();
+                m_instance.GetComponent<FieldManager>().enabled = true;
+            }
+            return m_instance;
+        }
+    }
+
+
+    void Start()
+    {
+        mapnumricvalue = new MapNumericValue();
+        layer = new Layer2D();
+        Create();
+      
+        for (int x=0;x<layer.Width;x++)
+        {
+            for (int y=0;y<layer.Height;y++)
+            {
+
+                /*
+                位置指定
+                動的にオブジェクトの生成
+                */
+
+                switch (layer.Get( x , y))
+                {
+                    case 0:
+                        Debug.Log(" ");
+                        break;
+                    case 1:
+                        /*
+                        //時間で表示
+                        Invoke("",5);
+                        */
+                        Debug.Log("W");
+                        break;
+                    case 2:
+                        Debug.Log("G");
+                        break;
+                    case 3:
+                        Debug.Log("P");
+                        break;
+                    case 4:
+                        Debug.Log("EN");
+                        break;
+                    case 5:
+                        Debug.Log("EX");
+                        break;
+
+                }
+            }
+        }
+        
+             
+    }
+   
+    public Layer2D Getlayer()
+    {
+        return layer;
+    }
+    public void Create()
+    {
+
+        mapnumricvalue.Create("RB01");
+   
+        layer = mapnumricvalue.GetLayer();
+    }
+    
+   
+}
---
+++ Assets/Scripts/Field/MapNumericValue.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Text;
+using System.Xml;
+using System.IO;
+using System;
+
+public class MapNumericValue
+{
+    private Layer2D layer = null;
+
+    /*
+    レベルデータを読み込む
+    */
+    public void Create(string path)
+    {
+        // レイヤー生成
+        layer = new Layer2D();
+        // レベルデータ取得
+        TextAsset tmx = Resources.Load("Levels/"+path) as TextAsset;
+
+        // XML解析開始
+        XmlDocument xmlDoc = new XmlDocument();
+        xmlDoc.LoadXml(tmx.text);
+        XmlNodeList mapList = xmlDoc.GetElementsByTagName("map");
+        foreach (XmlNode map in mapList)
+        {
+            XmlNodeList childList = map.ChildNodes;
+            foreach (XmlNode child in childList)
+            {
+                if (child.Name != "layer") { continue; } // layerノード以外は見ない
+
+                // マップ属性を取得
+                XmlAttributeCollection attrs = child.Attributes;
+                int w = int.Parse(attrs.GetNamedItem("width").Value); // 幅を取得
+                int h = int.Parse(attrs.GetNamedItem("height").Value); // 高さを取得
+                // レイヤー生成
+                layer.Create(w, h);
+                XmlNode node = child.FirstChild; // 子ノードは<data>のみ
+                XmlNode n = node.FirstChild; // テキストノードを取得
+                string val = n.Value; // テキストを取得
+                // CSV(マップデータ)を解析
+                int y = 0;
+                foreach (string line in val.Split('\n'))
+                {
+                    int x = 0;
+                    foreach (string s in line.Split(','))
+                    {
+                        int v = 0;
+                        // ","で終わるのでチェックが必要
+                        if (int.TryParse(s, out v) == false) { continue; }
+                        // 値を設定
+                        layer.Set(x, y, v);
+                        x++;
+                    }
+                    y++;
+                }
+               
+            }
+        }
+       
+        // デバッグ出力
+        layer.Dump();
+      
+
+    }
+   public Layer2D GetLayer()
+    {
+        return layer;
+    } 
+   
+}
