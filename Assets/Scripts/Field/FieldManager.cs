9cadcfe2ec743c39a5a3923423d4b04e0f6f9b2d
tomytomychoper <iwgm14clow526@gmail.com>
2016-06-22 14:48:44 +0900

    フィールド生成

---
+++ Assets/Scripts/Field/FieldManager.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System.Xml;
+using System.Text;
+using System.Collections;
+using System.IO;
+
+public class FieldManager : MonoBehaviour
+{
+
+    /*
+    FiledManager:フィールド管理
+    MapNumericValue:外部テキスト読み込み
+    Layer2D:データを格納しとく場所
+　　シングルトン化
+    */
+
+    private object mapData;
+    private MapNumericValue mapnumricvalue;
+    private Layer2D layer;
+    private static FieldManager m_instance = null;
+    public static FieldManager Instance
+    {
+        get
+        {
+            if (m_instance == null)
+            {
+                /*
+                compornet 　スクリプトからGameObjectにアタッチするため
+
+                */
+
+                var obj = new GameObject("FieldManager!!");
+                m_instance = obj.AddComponent<FieldManager>();
+                m_instance.GetComponent<FieldManager>().enabled = true;
+
+            }
+            return m_instance;
+        }
+    }
+
+
+    void Start()
+    {
+        mapnumricvalue = new MapNumericValue();
+        layer = new Layer2D();
+        Create();
+        ResourceManager.Instance.ResourcesLoad("Game");
+
+        for (int x = 0; x < layer.Width; x++)
+        {
+            for (int y = 0; y < layer.Height; y++)
+            {
+
+                /*
+                位置指定
+                動的にオブジェクトの生成
+                */
+
+                switch (layer.Get(x, y))
+                {
+                    case 0:
+                        //Debug.Log(" ");
+                        break;
+                    case 1:
+                        Instantiate(ResourceManager.Instance.GetResourceScene("Wall"), new Vector3(x, 1, y), Quaternion.identity);
+                        //Debug.Log("W");
+                        break;
+                    case 2:
+                        Instantiate(ResourceManager.Instance.GetResourceScene("Ground"), new Vector3(x, 0, y), Quaternion.identity);
+                        //Debug.Log("G");
+                        break;
+                    case 3:
+                        Instantiate(ResourceManager.Instance.GetResourceScene("Player"), new Vector3(x, 1, y), Quaternion.identity);
+                        Instantiate(ResourceManager.Instance.GetResourceScene("Ground"), new Vector3(x, 0, y), Quaternion.identity);
+                        //Debug.Log("P");
+                        break;
+                    case 4:
+                        Instantiate(ResourceManager.Instance.GetResourceScene("Enemy_a"), new Vector3(x, 1, y), Quaternion.identity);
+                        Instantiate(ResourceManager.Instance.GetResourceScene("Ground"), new Vector3(x, 0, y), Quaternion.identity);
+                        //Debug.Log("EN");
+                        break;
+                    case 5:
+                        //Debug.Log("EX");
+                        Instantiate(ResourceManager.Instance.GetResourceScene("Exite"), new Vector3(x, 0, y), Quaternion.identity);
+                        break;
+
+                }
+            }
+        }
+
+
+    }
+
+    public Layer2D Getlayer()
+    {
+        return layer;
+    }
+    public void Create()
+    {
+
+        mapnumricvalue.Create("RB01");
+
+        layer = mapnumricvalue.GetLayer();
+    }
+
+
+}
---
+++ Assets/Scripts/Field/Layer2D.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Layer2D
+{
+
+    private int width; // 幅
+    private int height; // 高さ
+    private int[] _vals = null; // マップデータ
+
+    public int Width
+    {
+        get
+        {
+            return width;
+        }
+    }
+    public int Height
+    {
+        get
+        {
+            return height;
+        }
+    }
+
+
+    // 作成
+    public void Create(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+        _vals = new int[width * height];
+
+
+
+    }
+
+    // 値の取得
+    // @param x X座標
+    // @param y Y座標
+    // @return 指定の座標の値 (領域外を指定したら-1)
+    public int Get(int x, int y)
+    {
+        if (x < 0 || x >= width) { return -1; }
+        if (y < 0 || y >= height) { return -1; }
+        return _vals[y * width + x];
+    }
+
+    // 値の設定
+    // @param x X座標
+    // @param y Y座標
+    // @param val 設定する値
+    public void Set(int x, int y, int val)
+    {
+        if (x < 0 || x >= width) { return; }
+        if (y < 0 || y >= height) { return; }
+        _vals[y * width + x] = val;
+    }
+
+    // デバッグ出力
+    public void Dump()
+    {
+        Debug.Log("[Layer2D] (w,h)=(" + width + "," + height + ")");
+        for (int y = 0; y < height; y++)
+        {
+            string s = "";
+            for (int x = 0; x < width; x++)
+            {
+                s += Get(x, y) + ",";
+            }
+            Debug.Log(s);
+        }
+    }
+
+    private void Getfirstgid()
+    {
+        throw new System.NotImplementedException();
+    }
+}
