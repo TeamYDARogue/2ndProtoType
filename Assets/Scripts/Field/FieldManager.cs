9de26c8719aa234a9e7b4fdd4bfb66e69cfe8351
unknown <kei@GM2140102.da.iwasaki.ac.jp>
2016-06-24 09:51:47 +0900

    FieldManagerの魔改造開始

--- Assets/Scripts/Field/FieldManager.cs
+++ Assets/Scripts/Field/FieldManager.cs
@@ -1,28 +1,23 @@
 ﻿using UnityEngine;
-using System.Xml;
-using System.Text;
 using System.Collections;
-using System.IO;
 
 public class FieldManager : MonoBehaviour
 {
-
     /*
     FiledManager:フィールド管理
     MapNumericValue:外部テキスト読み込み
     Layer2D:データを格納しとく場所
 　　シングルトン化
     */
-
     private object mapData;
-    private MapNumericValue mapnumricvalue;
+    private MapNumericValue mapNumricValue;
     private Layer2D layer;
-    private static FieldManager m_instance = null;
+    private static FieldManager instance = null;
     public static FieldManager Instance
     {
         get
         {
-            if (m_instance == null)
+            if (instance == null)
             {
                 /*
                 compornet 　スクリプトからGameObjectにアタッチするため
@@ -30,22 +25,38 @@ public class FieldManager : MonoBehaviour
                 */
 
                 var obj = new GameObject("FieldManager!!");
-                m_instance = obj.AddComponent<FieldManager>();
-                m_instance.GetComponent<FieldManager>().enabled = true;
+                instance = obj.AddComponent<FieldManager>();
+                instance.GetComponent<FieldManager>().enabled = true;
 
             }
-            return m_instance;
+            return instance;
         }
     }
 
 
     void Start()
     {
-        mapnumricvalue = new MapNumericValue();
-        layer = new Layer2D();
-        Create();
         ResourceManager.Instance.ResourcesLoad("Game");
+        mapNumricValue = new MapNumericValue();
+        layer = new Layer2D();
 
+        LoadStage("RB01");
+        StartCoroutine(CreateStage());
+    }
+
+    public Layer2D Getlayer()
+    {
+        return layer;
+    }
+
+    public void LoadStage(string mapName)
+    {
+        mapNumricValue.Create(mapName);
+        layer = mapNumricValue.GetLayer();
+    }
+
+    IEnumerator CreateStage()
+    {
         for (int x = 0; x < layer.Width; x++)
         {
             for (int y = 0; y < layer.Height; y++)
@@ -59,49 +70,29 @@ public class FieldManager : MonoBehaviour
                 switch (layer.Get(x, y))
                 {
                     case 0:
-                        //Debug.Log(" ");
                         break;
                     case 1:
                         Instantiate(ResourceManager.Instance.GetResourceScene("Wall"), new Vector3(x, 1, y), Quaternion.identity);
-                        //Debug.Log("W");
                         break;
                     case 2:
                         Instantiate(ResourceManager.Instance.GetResourceScene("Ground"), new Vector3(x, 0, y), Quaternion.identity);
-                        //Debug.Log("G");
                         break;
                     case 3:
                         Instantiate(ResourceManager.Instance.GetResourceScene("Player"), new Vector3(x, 1, y), Quaternion.identity);
                         Instantiate(ResourceManager.Instance.GetResourceScene("Ground"), new Vector3(x, 0, y), Quaternion.identity);
-                        //Debug.Log("P");
                         break;
                     case 4:
                         Instantiate(ResourceManager.Instance.GetResourceScene("Enemy_a"), new Vector3(x, 1, y), Quaternion.identity);
                         Instantiate(ResourceManager.Instance.GetResourceScene("Ground"), new Vector3(x, 0, y), Quaternion.identity);
-                        //Debug.Log("EN");
                         break;
                     case 5:
-                        //Debug.Log("EX");
                         Instantiate(ResourceManager.Instance.GetResourceScene("Exite"), new Vector3(x, 0, y), Quaternion.identity);
                         break;
 
                 }
             }
         }
-
-
+        yield return null;
     }
-
-    public Layer2D Getlayer()
-    {
-        return layer;
-    }
-    public void Create()
-    {
-
-        mapnumricvalue.Create("RB01");
-
-        layer = mapnumricvalue.GetLayer();
-    }
-
 
 }
