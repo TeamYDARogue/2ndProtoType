68b317181a4ba45d8d88b29f42d2ecb5c8d3fd81
IwaIwapong <takuma_8.3.6@ezweb.ne.jp>
2016-07-04 09:17:59 +0900

    縦横verと斜めverのソース更新です。斜めはまだ違和感有り。

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -3,85 +3,91 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour {
 
-
-	//private Vector3 originPosition = Vector3.zero;
-	//private TweenMove tweenMove = null;
-
 	private GameObject m_target;  //最も近いオブジェクト
 	private float m_time = 0;     //経過時間
 
-	void Start()
-	{
-		//最も近かったオブジェクトを取得する処理
-		m_target = serchTag(gameObject, "Player");
-
-		//originPosition = transform.position;
-		//tweenMove = Tween.Play ("EnemyTween") as TweenMove;
-		//tweenMove.Stop ();
+	void Start(){
+		//最も近いオブジェクトを取得
+		this.m_target = this.serchTag (gameObject, "Player");
 	}
 
-	void Update()	
-	{
-		    //経過時間を取得する処理
-			m_time += Time.deltaTime;
+	void Update(){
 
-			if (m_time >= 1.0f) {
-			//最も近かったオブジェクトを取得する処理
-				m_target = serchTag (gameObject, "Player");
+		this.m_time += Time.deltaTime;   //経過時間を取得
+
+		if (this.m_time >= 1.0f) {
+			//最も近いオブジェクトを取得
+			this.m_target = this.serchTag (gameObject, "Player");
 			//経過時間を初期化
-				m_time = 0;
+			this.m_time = 0;
+
+			//移動のスピード
+			this.changeDirection (this.m_target);  //向かう方向に向く
+			this.transform.Translate (Vector3.forward * 2.00f);    //向かう方向に向かう
 		}
-
-		/*if(Input.GetKeyDown(KeyCode.A)){
-			transform.position = new Vector3 (3.0f, 0.0f, 0.0f);
-		}
-
-		if (Input.GetKeyDown (KeyCode.D)) {
-			transform.position += new Vector3 (3.0f, 0.0f, 0.0f);
-		}
-
-		if (Input.GetKeyDown (KeyCode.W)) {
-			transform.position += new Vector3 (0.0f, 0.0f, 3.0f);
-		}
-
-		if (Input.GetKeyDown (KeyCode.S)) {
-			transform.position -= new Vector3 (0.0f, 0.0f, 3.0f);
-		}*/
-
-		//Zキーを押したら
-		if (Input.GetKeyDown (KeyCode.Z)) {
-			transform.LookAt (m_target.transform);            //キャラの方向に向く
-			transform.Translate (Vector3.forward * 2.00f);    //キャラの方に向かう
-		}
-
-		/*if (Input.GetKeyDown (KeyCode.A)) 
-		{
-			Debug.Log("TWeen!");
-			OriginEnemy();
-		}*/
 	}
 
-	/*void OriginEnemy()
-	{
-		tweenMove.startPosition = transform.position; 
-		tweenMove.targetPosition = originPosition;    
-		tweenMove.Play ();
-	}*/
+	//Playerの座標に応じて向きを変える
+	void changeDirection(GameObject player){
 
-	//指定されたタグの中で最も近い物を取得する処理
+		//PlayerとEnemyの座標差分を取得
+		int m_xDistance = Mathf.RoundToInt (this.transform.position.x - player.transform.position.x);
+		int m_zDistance = Mathf.RoundToInt (this.transform.position.z - player.transform.position.z);
+
+		//向きたい角度
+		int m_rotateDir = 0;
+
+		//x座標とy座標の差分から向きたい角度を取得
+		//PlayerとEnemyに距離がある場合
+		if (m_xDistance == 0) {
+			//x座標が同じ場合,z座標のみ向きを取得
+			m_rotateDir = this.getDirection (m_zDistance, "z");
+		} else if (m_zDistance == 0) {
+			//z座標が同じ場合,x座標のみ向きを取得
+			m_rotateDir = this.getDirection (m_xDistance, "x");
+		} else {
+			//どちらも差がある場合,ランダムで進む向きを取得
+			int m_rand = UnityEngine.Random.Range (0, 2);
+			if (m_rand == 0) {
+				//z座標
+				m_rotateDir = this.getDirection (m_zDistance, "z");
+			} else {
+				//x座標
+				m_rotateDir = this.getDirection (m_xDistance, "x");
+			}
+		}
+
+		//取得した方向にオブジェクトの向きを変える
+		this.transform.rotation = Quaternion.Euler (0, m_rotateDir, 0);
+
+	}
+
+	//向きの角度を取得
+	int getDirection(int distance,string axis){
+		//距離がプラスかマイナスを取得
+		int flag = distance > 0 ? 1 : 0;
+
+		//角度を返す
+		if (axis == "x") {
+			return flag == 1 ? 270 : 90;
+		} else {
+			return flag == 1 ? 180 : 0;
+		}
+	}
+
+
 	GameObject serchTag(GameObject nowObj,string tagName){
 
-		float tmpDis = 0;       //距離用一時変数
-		float nearDis = 0;      //最も近いオブジェクトとの距離
-
+		float tmpDis = 0;     //距離用の一時変数
+		float nearDis = 0;    //最も近いオブジェクトの距離
 		GameObject targetObj = null;     //オブジェクト
 
-		//タグ指定されたプレイヤーを配列で取得
+		//タグ指定されたオブジェクトを配列で取得
 		foreach (GameObject obs in GameObject.FindGameObjectsWithTag(tagName)) {
-			//Enemyとプレイヤーの距離を取得
+			//Enemyと取得したオブジェクトの距離を取得
 			tmpDis = Vector3.Distance (obs.transform.position, nowObj.transform.position);
 
-			//プレイヤーとの距離が近いか,距離0であればオブジェクト名を取得
+			//オブジェクトの距離が近いか,距離0であればオブジェクト名を取得
 			//一時変数に距離を格納
 			if (nearDis == 0 || nearDis > tmpDis) {
 				nearDis = tmpDis;
@@ -89,49 +95,16 @@ public class Enemy : MonoBehaviour {
 			}
 		}
 
-		//最も近かったオブジェクトを返す
+		//最も近いオブジェクトを返す
 		return targetObj;
 	}
 
-
-
-
-
-
-	/*private Transform m_target;
-	private bool m_move;
-
-	protected override void Start()
-	{
-		m_target = GameObject.FindGameObjectWithTag ("Player").transform;
-		base.Start ();
-	}
-
-	protected override void AttemptMove<T> (int xDir, int yDir)
-	{
-		if (m_move) {
-			m_move = false;
-			return;
+	//当たり判定
+	void OnTriggerEnter(Collider col){
+		//Playerに当たった時
+		if (col.gameObject.tag == "Player") {
+			Debug.Log ("攻撃");
 		}
-		base.AttemptMove <T> (xDir, yDir);
-		m_move = true;
 	}
 
-	public void MobeEnemy()
-	{
-		int xDir = 0;
-		int yDir = 0;
-		if (Mathf.Abs (m_target.position.x - transform.position.x) < float.Epsilon) {
-			yDir = m_target.position.y > transform.position.y ? 1 : -1;
-		} else {
-			xDir = m_target.position.x > transform.position.x ? 1 : -1;
-		}
-
-		AttemptMove <Player> (xDir, yDir);
-	}
-
-	protected override void OnCantMove <T> (T component)
-	{
-		Player hitPlayer = component as Player;
-	}*/
 }
