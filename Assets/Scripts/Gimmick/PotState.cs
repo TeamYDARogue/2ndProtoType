b714c5f9ff2400fbb5ab0d2608841de9282dac41
ayakohosoyaa <ruukaionnji12@yahoo.com>
2016-07-05 16:39:25 +0900

    槍、感圧版、壺のスクリプトを追加

---
+++ Assets/Scripts/Gimmick/PotState.cs
@@ -0,0 +1,180 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PotState : PlayerMover {
+
+    private Rigidbody m_RigidBody;
+    [SerializeField]
+    private GameObject m_Pot;
+    [SerializeField]
+    private GameObject m_Player;
+    private Vector3 m_PotPos;
+    private Vector3 m_Playerpos;
+    private Vector3 m_poss;
+    private State m_State = State.NoTacch;
+    private PlayerMover m_PlayerMover;
+
+    void Start()
+    {
+        m_RigidBody = GetComponent<Rigidbody>();
+        m_PotPos = m_Pot.transform.position;
+        m_PlayerMover = GetComponent<PlayerMover>();
+        
+    }
+    void FixedUpdate()
+    {
+        pos();
+        m_Playerpos = m_Player.transform.position;
+    }
+    void pos()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            m_Pot.transform.Translate(0, 0, -1);
+        }
+        m_Playerpos = m_Player.transform.position;
+        m_PotPos = m_Pot.transform.position;
+        m_poss = m_Playerpos - m_PotPos;
+
+        Debug.Log(m_poss);
+        Debug.Log(m_State);
+
+        if (m_State == State.NoTacch)
+        {
+            if (m_poss.x == 1 && m_poss.z == 0)
+            {
+                Input.ResetInputAxes();
+                ChangeState(State.LeftTacch);
+            }
+            else if (m_poss.x == -1 && m_poss.z == 0)
+            {
+                Input.ResetInputAxes();
+                ChangeState(State.RightTacch);
+            }
+            else if (m_poss.z == 1 && m_poss.x == 0)
+            {
+                Input.ResetInputAxes();
+                ChangeState(State.DownTacch);
+            }
+            else if (m_poss.z == -1 && m_poss.x == 0)
+            {
+                Input.ResetInputAxes();
+                ChangeState(State.UpTacch);
+            }
+        }
+        switch (m_State)
+        {
+            case State.RightTacch:
+                if (Input.GetKeyDown(KeyCode.LeftArrow))
+                {
+                    m_Pot.SetActive(false);
+                    //m_RigidBody.constraints = (RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ);
+                    m_Pot.transform.position = new Vector3(100, 100, 100);
+                    m_PotPos = m_Pot.transform.position;
+                }
+                else if (Input.GetKeyDown(KeyCode.LeftArrow) ||
+                        Input.GetKeyDown(KeyCode.UpArrow) ||
+                        Input.GetKeyDown(KeyCode.DownArrow))
+                {
+                    ChangeState(State.WasTacch);
+                }
+                break;
+            case State.LeftTacch:
+                if (Input.GetKeyDown(KeyCode.RightArrow))
+                {
+                    m_Pot.SetActive(false);
+                    m_Pot.transform.position = new Vector3(100, 100, 100);
+                    m_PotPos = m_Pot.transform.position;
+                }
+                else if (Input.GetKeyDown(KeyCode.RightArrow) ||
+                         Input.GetKeyDown(KeyCode.UpArrow) ||
+                         Input.GetKeyDown(KeyCode.DownArrow))
+                {
+                    ChangeState(State.WasTacch);
+                }
+                break;
+            case State.UpTacch:
+                if (Input.GetKeyDown(KeyCode.DownArrow))
+                {
+                    m_Pot.SetActive(false);
+                    m_Pot.transform.position = new Vector3(100, 100, 100);
+                    m_PotPos = m_Pot.transform.position;
+                }
+                else if (Input.GetKeyDown(KeyCode.LeftArrow) ||
+                         Input.GetKeyDown(KeyCode.RightArrow) ||
+                         Input.GetKeyDown(KeyCode.DownArrow))
+                {
+                    ChangeState(State.WasTacch);
+                }
+                break;
+            case State.DownTacch:
+                if (Input.GetKeyDown(KeyCode.UpArrow))
+                {
+                    //m_RigidBody.constraints = (RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ);
+                    //m_Pot.transform.position = new Vector3(100, 100, 100);
+                    m_Pot.SetActive(false);
+                    m_PotPos = m_Pot.transform.position;
+                    ChangeState(State.NoTacch);
+                }
+                else if (Input.GetKeyDown(KeyCode.LeftArrow) ||
+                         Input.GetKeyDown(KeyCode.UpArrow) ||
+                         Input.GetKeyDown(KeyCode.RightArrow))
+                {
+                    ChangeState(State.WasTacch);
+                }
+                break;
+            default:
+                break;
+        }
+        if (m_State == State.UpTacch ||
+           m_State == State.DownTacch ||
+           m_State == State.RightTacch ||
+           m_State == State.LeftTacch)
+        {
+            if (m_poss.x == 100 || m_poss.x == -100 ||
+                m_poss.z == 100 || m_poss.z == -100)
+            {
+                ChangeState(State.NoTacch);
+                //m_RigidBody.constraints = (RigidbodyConstraints.None);
+                //m_PlayerMover.Update();
+            }
+        }
+        if (m_State == State.WasTacch)
+        {
+            
+            if (m_poss.z != -1 && m_poss.x != 0 || m_poss.z != 1 && m_poss.x != 0 ||
+                m_poss.x != -1 && m_poss.z != 0 || m_poss.x != 1 && m_poss.z != 0)
+            {
+                //m_RigidBody.constraints = (RigidbodyConstraints.None);
+                ChangeState(State.NoTacch);
+            }
+
+        }
+    }
+    void OnCollisionEnter(Collision c)
+    {
+        Debug.Log("bbbbbBBBBBBBBBBBBBBBBBB");
+
+        if (c.gameObject.tag == "Player")
+        {
+            Debug.Log("CCCCCCCCCCCCCCCCC");
+            m_Pot = c.gameObject;
+            Debug.Log("Aaaaaaaaaaaaaaaaaa");
+        }
+
+    }
+    public enum State
+    {
+        UpTacch,
+        DownTacch,
+        LeftTacch,
+        RightTacch,
+        NoTacch,
+        WasTacch,
+    }
+    public void ChangeState(State s)
+    {
+        m_State = s;
+    }
+}
---
+++ Assets/Scripts/Gimmick/PressPlate.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressPlate : MonoBehaviour {
+
+
+    private Renderer m_Rend;
+    private State m_PressState = State.NoTouchPress;    //感圧版の状態管理
+    [SerializeField]
+    private GameObject spearObj = null;
+    private Spear spear = null;
+
+    void Start()
+    {
+        m_Rend = GetComponent<Renderer>();
+        spear = spearObj.GetComponent<Spear>();
+    }
+    public void UpdateHandler()
+    {
+
+    }
+    void OnCollisionEnter(Collision C)
+    {
+        if (C.gameObject.tag == "Player" && m_PressState == State.NoTouchPress)
+        {
+            Debug.Log("触れている");
+            //色を変える
+            m_Rend.material.EnableKeyword("_Color");
+            m_Rend.material.SetColor("s_Color", new Color(0, 1, 0));
+            //感圧版を触れている状態に切り替える
+            ChangeState(State.TouchPress);
+            //ドアを開ける
+            spear.SpearState();
+        }
+    }
+    /*プレイヤーが感圧版に触れていないとき*/
+    /*void OnCollisionExit(Collision Exit)
+    {
+        if (Exit.gameObject.tag == "Player" && m_PressState == State.TouchPress)
+        {
+            Debug.Log("触れていない");
+            //色を変える
+            m_Rend.material.EnableKeyword("_Color");
+            m_Rend.material.SetColor("_Color", new Color(1, 1, 1));
+            //感圧版を触れていない状態に切り替える
+            ChangeState(State.NoTouchPress);
+            //ドアを閉める
+            spear.SpearState();
+        }
+
+    }*/
+    public enum State
+    {
+        TouchPress,
+        NoTouchPress,
+    }
+    public void ChangeState(State s)
+    {
+        m_PressState = s;
+    }
+}
